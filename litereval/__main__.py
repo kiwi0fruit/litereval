44247943fcdfdc6c399d98887a9cbf71056359e0
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-01-17 14:42:12 +0700

    commit

--- litereval/__main__.py
+++ litereval/__main__.py
@@ -1,38 +1,53 @@
 import sys
 import ast
-import copy
+from copy import deepcopy
 import re
+
+obj = {'gg': '676', None: 0, (): (1, 2, 3), 'a': """{-1: "123", foo='bar'}
+""",
+ 'x': [1, 2, {'foo': 33, 'bar': '{x=1, y=2, \
+z=3}'}]}  # noqa
+
+str_ = """\
+{gg='676', None: 0, (): (1, 2, 3), a=\"\"\"{-1: "123", foo='bar'}
+\"\"\",
+ x=[1, 2, {foo=33, bar='{x=1, y=2, \\
+z=3}'}]}\
+"""
 
 
 def litereval(string: str):
     """
     Small extension of ``ast.literal_eval`` that also
     accepts dict in a form of ``{key=100, foo='bar'}``
+
+    >>> assert repr(obj) == repr(litereval(str_))
     """
     input_charset = set(list(string))
     reps = []
-    
+
     for ch in range(256, sys.maxunicode):
         if not (chr(ch) in input_charset):
             reps.append(chr(ch))
             if len(reps) == 3:
                 break
 
-    reps = {'\\': reps[0], '"': reps[1], "'": reps[2]}
+    reps = {'\\': reps[0], '"': reps[1], "'": reps[2]}  # language=PythonRegExp
+    no_esc = r"(^|(?<=[^\\]))"
     string = re.sub(
-        r"(^|(?<=[^\\]))\\[\\'\"]",
+        rf"{no_esc}\\[\\'\"]",
         lambda m: reps[m.group(0)[1:]],
         string
     )
     string = re.sub(
-        r"(^|(?<=[^\\]))('''(.*?[^\\'])?'''|\"\"\"(.*?[^\\\"])?\"\"\"|'(.*?[^\\'])?'|\"(.*?[^\\\"])?\"|\w+[=]((?=[^=])|$))",
+        rf"{no_esc}('''(.*?[^\\'])?'''|\"\"\"(.*?[^\\\"])?\"\"\"|'(.*?[^\\'])?'|\"(.*?[^\\\"])?\"|\w+[=]((?=[^=])|$))",
         lambda m: f'"{m.group(0)[:-1]}": ' if (m.group(0)[-1] == '=') else m.group(0),
         string,
         flags=re.DOTALL
     )
     reps = {val: key for key, val in reps.items()}
     string = re.sub(
-        r"[{''.join(map(re.escape, reps.keys()))}]",
+        rf"[{''.join(map(re.escape, reps.keys()))}]",
         lambda m: reps[m.group(0)],
         string
     )
@@ -40,33 +55,31 @@ def litereval(string: str):
 
 
 def merge(source: dict, destination: dict,
-              deepcopy: bool=False):
+          copy: bool = False):
     """
     Deep merge two dictionaries.
     Overwrites in case of conflics.
     From https://stackoverflow.com/a/20666342
 
-    >>> dst = {'first': {
-    >>>     'inn': {'foo': 'dog', 'n': 1}
-    >>> }}
-    >>> src = {'first': {
-    >>>     'inn': {'bar': 'cat', 'n': 5}
-    >>> }}
-    >>> merge(src, dst) == {'first': {
-    >>>     'inn': {'foo': 'dog', 'bar': 'cat', 'n': 5}
-    >>> }}
-    True
+    >>> dst = {'first': {'inn': {'foo': 'dog', 'n': 1}}}
+    >>> repr_dst = repr(dst)
+    >>> src = {'first': {'inn': {'bar': 'cat', 'n': 5}}}
+    >>> repr_src = repr(src)
+    >>> ret = {'first': {'inn': {'foo': 'dog', 'n': 5, 'bar': 'cat'}}}
+    >>> assert repr(merge(src, dst, copy=True)) == repr(ret)
+    >>> assert (repr(dst) == repr_dst) and (repr(src) == repr_src)
+    >>> assert repr(merge(src, dst)) == repr(ret)
+    >>> assert not (repr(dst) == repr_dst) and (repr(src) == repr_src)
     """
-    if deepcopy:
-        destination = copy.deepcopy(destination)
+    if copy:
+        destination = deepcopy(destination)
 
     for key, val in source.items():
         if isinstance(val, dict):
             # get node or create one
             node = destination.setdefault(key, {})
-            merge(val, node, deepcopy)
+            merge(val, node, copy=False)
         else:
-            destination[key] = (copy.deepcopy(val)
-                                if deepcopy else val)
+            destination[key] = (deepcopy(val) if copy else val)
 
     return destination
